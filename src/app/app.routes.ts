bbf0daafee22348f2ac2bbf57f81efbe8843a424
Lari-xavier <larissa.xavier@univale.br>
2025-08-29 02:04:11 -0300

    home screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,26 +3,41 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent)
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => 
+      import('./pages/auth/login/login.component').then(m => m.LoginComponent)
   },
   {
     path: 'forgot-password',
-    loadComponent: () => import('./pages/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+    loadComponent: () => 
+      import('./pages/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
   },
   {
     path: 'forgot-password/sent',
-    loadComponent: () => import('./pages/auth/forgot-password-sent/forgot-password-sent.component').then(m => m.ForgotPasswordSentComponent)
+    loadComponent: () => 
+      import('./pages/auth/forgot-password-sent/forgot-password-sent.component').then(m => m.ForgotPasswordSentComponent)
   },
   {
     path: 'create-password/:token',
-    loadComponent: () => import('./pages/auth/create-password/create-password.component').then(m => m.CreatePasswordComponent)
+    loadComponent: () => 
+      import('./pages/auth/create-password/create-password.component').then(m => m.CreatePasswordComponent)
   },
   {
     path: 'reset-password/:token',
-    loadComponent: () => import('./pages/auth/reset-password/reset-password.component').then(m => m.ResetPasswordComponent)
+    loadComponent: () => 
+      import('./pages/auth/reset-password/reset-password.component').then(m => m.ResetPasswordComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => 
+      import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'dashboard'
   }
 ];
