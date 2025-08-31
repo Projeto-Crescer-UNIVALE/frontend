bcc774925625a7649cd165e21cf7c5e9766ff6da
pedrocruzg <pedroaugustogomes61@gmail.com>
2025-08-31 15:00:58 -0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { PainelComponent } from './pages/painel/painel.component';
+import { AlunosComponent } from './pages/alunos/alunos.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { OficinasComponent } from './pages/oficinas/oficinas.component';
+
+export const routes: Routes = [
+    { path: 'painel', component: PainelComponent },
+    { path: 'alunos', component: AlunosComponent },
+    { path: 'usuarios', component: UsuariosComponent },
+    { path: 'oficinas', component: OficinasComponent },
+
+    { path: '', redirectTo: '/painel', pathMatch: 'full' }
+];
