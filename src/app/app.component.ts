73e7e58a60c92bfd7fb70ab9ca43af8166975e8f
Lari-xavier <larissa.xavier@univale.br>
2025-09-11 23:31:34 -0300

    atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OficinasListComponent } from './pages/oficinas/oficinas-list/oficinas-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // ✅ precisa estar aqui
+  imports: [RouterOutlet, OficinasListComponent], // ✅ adicione seu componente aqui
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // ✅ corrigido (plural)
 })
 export class AppComponent {
   title = 'repositorio-front';
