f06774648797bf3c9ba19cc8a572a86e97e0ca6b
Lari-xavier <larissa.xavier@univale.br>
2025-09-16 22:57:18 -0300

    validação dos campos

--- src/app/pages/oficinas/oficina-form/oficina-form.component.ts
+++ src/app/pages/oficinas/oficina-form/oficina-form.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, ActivatedRoute, Router } from '@angular/router';
-import { ReactiveFormsModule, FormsModule , FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule , FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
-type Dia = {
-  nome: string;
-  key: string;
-  checked: boolean;
-  inicio?: string;
-  fim?: string;
-  bloqueado?: boolean; 
-};
+interface DiarioCronograma {
+  dia: number;
+  hora_inicio: string;
+  hora_fim: string;
+}
+
+interface CronogramaFormArray {
+  dia: FormControl<number>;
+  hora_inicio: FormControl<string>;
+  hora_fim: FormControl<string>;
+}
 
 @Component({
   selector: 'app-oficina-form',
@@ -18,26 +21,33 @@ type Dia = {
 
   imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule],
   templateUrl: './oficina-form.component.html',
-  styleUrls: ['./oficina-form.component.css']
+  styleUrls: ['./oficina-form.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OficinaFormComponent {
   id: string | null;
   titulo: string;
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+  
 
 
   readonly form = new FormGroup({
-    nome: new FormControl('', [Validators.required]),
-    professor: new FormControl('', [Validators.required])
+    nome: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+    id_funcionario: new FormControl('', [Validators.required]),
+    descricao: new FormControl('', [Validators.required, Validators.maxLength(250)]),
+    status: new FormControl(true, [Validators.required]),
+    cronograma: new FormArray<FormGroup<CronogramaFormArray>>([]),
+
   });
 
-  dias: Dia[] = [
-    { nome: 'Domingo', key: 'dom', checked: true, inicio: '00:00', fim: '00:00' },
-    { nome: 'Segunda-feira', key: 'seg', checked: false, bloqueado: true },
-    { nome: 'Terça-feira', key: 'ter', checked: false, bloqueado: true },
-    { nome: 'Quarta-feira', key: 'qua', checked: false, bloqueado: true },
-    { nome: 'Quinta-feira', key: 'qui', checked: false, bloqueado: true },
-    { nome: 'Sexta-feira', key: 'sex', checked: false, bloqueado: true },
-    { nome: 'Sábado', key: 'sab', checked: false, bloqueado: true },
+  readonly dias = [
+    { nome: 'Domingo'},
+    { nome: 'Segunda-feira'},
+    { nome: 'Terça-feira'},
+    { nome: 'Quarta-feira'},
+    { nome: 'Quinta-feira'},
+    { nome: 'Sexta-feira'},
+    { nome: 'Sábado'},
   ];
 
   constructor(private route: ActivatedRoute, private router: Router) {
@@ -45,12 +55,45 @@ export class OficinaFormComponent {
     this.titulo = this.id ? 'Editar oficina' : 'Adicionar nova oficina';
 
   
-    if (this.id) {
-      this.form.patchValue({
-        nome: 'Oficina de Música',
-        professor: 'João da Silva'
-      });
+    // if (this.id) {
+    //   this.form.patchValue({
+    //     nome: 'Oficina de Música',
+    //     professor: 'João da Silva'
+    //   });
+    // }
+  }
+
+  createCronogramaControl(cronograma?: DiarioCronograma){
+    return new FormGroup({
+      dia: new FormControl(cronograma?.dia ?? 1, { validators: [Validators.required], nonNullable: true }),
+      hora_inicio: new FormControl(cronograma?.hora_inicio ?? '00:00', { validators: [Validators.required], nonNullable: true }),
+      hora_fim: new FormControl(cronograma?.hora_fim ?? '00:00', { validators: [Validators.required], nonNullable: true }),
+    });
+  }
+
+  handleToggleCronograma(dia: number, event: Event) {
+    const isChecked = (event.target as HTMLInputElement).checked;
+
+    const cronogramaExists =this.cronogramaDiaExists(dia);
+
+    if (isChecked && !cronogramaExists) {
+      this.form.controls.cronograma.push(this.createCronogramaControl({ dia, hora_inicio: '00:00', hora_fim: '00:00'}));
     }
+
+    if (!isChecked && cronogramaExists) {
+     const index = this.form.controls.cronograma.value.findIndex(c => c.dia === dia);
+
+      if (index !== -1) {
+        this.form.controls.cronograma.removeAt(index);
+      }
+    }
+    this.changeDetectorRef.markForCheck();
+  }
+
+  cronogramaDiaExists(dia: number): boolean {
+    const cronogramas = this.form.controls.cronograma.value;
+
+    return cronogramas.some(c => c.dia === dia);
   }
 
   salvar() {
